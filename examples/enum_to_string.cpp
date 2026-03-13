7ae08ca67eb743f66549311b7efea3fa58322c26
Andrei.Tokmakov <andtokm@gmail.com>
2026-03-13 21:16:32 +0400

    13_03_2026_004

---
+++ examples/enum_to_string.cpp
@@ -0,0 +1,37 @@
+#include <meta>
+#include <string>
+#include <type_traits>
+#include <print>
+
+
+template<typename E, bool Enumerable = is_enumerable_type(^^E)>
+  requires std::is_enum_v<E>
+constexpr std::string_view enum_to_string(E value) {
+    if constexpr (Enumerable)
+        template for (constexpr auto e : define_static_array(enumerators_of(^^E)))
+            if (value == [:e:])
+                return identifier_of(e);
+
+    return "<unnamed>";
+}
+
+int main() {
+    enum Color : int;
+    static_assert(enum_to_string(Color(0)) == "<unnamed>");
+    std::println("Color 0: {}", enum_to_string(Color(0)));  // prints '<unnamed>'
+
+    enum Color : int { red, green, blue };
+    static_assert(enum_to_string(Color::red) == "red");
+    static_assert(enum_to_string(Color(42)) == "<unnamed>");
+    std::println("Color 0: {}", enum_to_string(Color(0)));  // prints 'red'
+}
+
+// https://godbolt.org/z/4s1deeaEv
+
+/*
+ASM generation compiler returned: 0
+Execution build compiler returned: 0
+Program returned: 0
+Color 0: <unnamed>
+Color 0: red
+*/
---
+++ examples/list_of_types_sizes.cpp
@@ -0,0 +1,15 @@
+#include <meta>
+#include <array>
+#include <algorithm>
+
+constexpr std::array types = {^^int, ^^float, ^^double};
+
+constexpr std::array sizes = []() {
+    std::array<std::size_t, types.size()> r;
+    std::transform(types.begin(), types.end(), r.begin(), std::meta::size_of);
+    return r;
+}();
+
+static_assert(sizes[0] == sizeof(int));
+static_assert(sizes[1] == sizeof(float));
+static_assert(sizes[2] == sizeof(double));
