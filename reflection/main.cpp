6dded4c686594f75668d5aa06e21deec8aa63da1
Andrei.Tokmakov <andtokm@gmail.com>
2026-03-22 09:51:58 +0400

    22_03_2026_001

--- reflection/main.cpp
+++ reflection/main.cpp
@@ -7,18 +7,41 @@ Copyright   : Your copyright notice
 Description : Coroutines
 ============================================================================**/
 
-#include <vector>
-#include <string_view>
-
 #include <iostream>
-#include <optional>
-#include <type_traits>
-#include <array>
-#include <string>
-#include <unordered_map>
-#include <ranges>
+#include <syncstream>
+#include <iomanip>
+#include <format>
+#include <print>
 
-#include <experimental/meta>
+#include <bitset>
+
+#include <fstream>
+#include <thread>
+#include <functional>
+#include <memory>
+#include <concepts>
+#include <utility>
+#include <string_view>
+#include <string>
+#include <cassert>
+#include <random>
+#include <tuple>
+#include <bit>
+#include <utility>
+#include <condition_variable>
+#include <source_location>
+
+#include <algorithm>
+#include <ranges>
+#include <set>
+#include <map>
+#include <vector>
+#include <deque>
+#include <unordered_set>
+#include <unordered_map>
+
+#include <coroutine>
+#include <meta>
 
 // https://godbolt.org/z/6Prs3fT1b
 
@@ -26,32 +49,6 @@ Description : Coroutines
 // ==============================================================================================================================
 // https://medium.com/@massimiliano.bastia92/c-static-reflection-an-overview-of-the-metaprogramming-paradigm-shift-4cc2ca49a2c6
 // ==============================================================================================================================
-
-struct User
-{
-    std::string name;
-    int age;
-};
-
-constexpr int max_connections = 100;
-
-void say_hello() { }
-
-
-void reflectTypes_Simple()
-{
-    // Reflect the struct type 'User'
-   constexpr std::meta::info user_reflection = ^^User;
-
-   // Reflect the global variable 'max_connections'
-   constexpr std::meta::info connections_reflection = ^^max_connections;
-
-   // Reflect the free function 'say_hello'
-   constexpr std::meta::info hello_reflection = ^^say_hello;
-
-   // Reflect the data member 'age' inside struct 'User'
-   constexpr std::meta::info user_age_reflection = ^^User::age;
-}
 
 // The [: :] Splicer
 // Once an entity has been reflected into a std::meta::info value, it can be reintroduced into the program’s code using the splicer syntax [: :].
@@ -69,25 +66,6 @@ void reflectTypes_Simple()
 //      typename [: r :] -  produces a simple type specifier if r reflects a type;
 //      template [: r :] -  produces a template name if r reflects a template.
 //      [: r :]::        -  produces a nested-name-specifier corresponding to a namespace, enumeration type, or class.
-
-void useReflectedData()
-{
-    constexpr std::meta::info int_reflection = ^^int;
-    constexpr std::meta::info double_reflection = ^^double;
-    constexpr std::meta::info string_reflection = ^^std::string;
-
-    typename [: int_reflection :] number = 100;
-    typename [: double_reflection :] pi = 3.14;
-    typename [: string_reflection :] message = "Hello Reflection";
-
-    std::cout << "Number: " << number << std::endl;
-    std::cout << "Pi: " << pi << std::endl;
-    std::cout << "Message: " << message << std::endl;
-
-    // Number: 100
-    // Pi: 3.14
-    // Message: Hello Reflection
-}
 
 
 template <typename T>
@@ -181,27 +159,15 @@ int main()
     create_new_type();
 }
 
-// ==============================================================================================================================
-// ==============================================================================================================================
 
 #endif
-
-
-#if 0
-
-constexpr auto r = ^^int;
-typename[:r:] x = 42;       // Same as: int x = 42;
-typename[:^^char:] c = '*';  // Same as: char c = '*';
-
-#endif
-
 
 
 #if 0
 namespace demo1
 {
-    #include <experimental/reflect>
-    using namespace std::experimental::reflect;
+    // #include <experimental/reflect>
+    // using namespace std::experimental::reflect;
 
     enum Color {
         Red,
@@ -211,7 +177,7 @@ namespace demo1
 
     using MetaT = reflexpr(Color);
 
-    int main() {
+    void test() {
         constexpr std::string_view name = get_name_v<get_element_t<0, get_enumerators_t<MetaT>>>;
         std::cout << "The name of the first value is \"" << name << "\"" << std::endl;
     }
@@ -361,8 +327,172 @@ namespace demo3
 #endif
 
 
-#if 0
-namespace print_enum_values_2
+
+
+namespace reflection
+{
+    struct User
+    {
+        std::string name;
+        int age { 0 };
+    };
+
+    enum class RGB {
+        Red,
+        Blue,
+        Green
+    };
+
+    enum class Color {
+        Red,
+        Green,
+        Blue,
+        Yellow,
+        Purple
+    };
+
+    enum class Animal {
+        Cat,
+        Dog,
+        Horse,
+        Rabbit,
+        Snail
+    };
+}
+
+namespace reflection::types
+{
+    constexpr int max_connections = 100;
+
+    void say_hello() { }
+
+
+    void basics()
+    {
+        constexpr auto r = ^^int;
+
+        typename[:r:] i = 42;       // Same as: int x = 42;
+        static_assert(std::is_same_v<decltype(i), int>);
+
+        typename[:^^char:] c = '*';  // Same as: char c = '*';
+        static_assert(std::is_same_v<decltype(c), char>);
+    }
+
+    void reflectTypes_Simple()
+    {
+        // Reflect the struct type 'User'
+         [[maybe_unused]]
+        constexpr std::meta::info user_reflection = ^^User;
+
+        // Reflect the global variable 'max_connections'
+         [[maybe_unused]]
+        constexpr std::meta::info connections_reflection = ^^max_connections;
+
+        // Reflect the free function 'say_hello'
+         [[maybe_unused]]
+        constexpr std::meta::info hello_reflection = ^^say_hello;
+
+        // Reflect the data member 'age' inside struct 'User'
+         [[maybe_unused]]
+        constexpr std::meta::info user_age_reflection = ^^User::age;
+    }
+
+    void useReflectedData()
+    {
+        constexpr std::meta::info int_reflection = ^^int;
+        constexpr std::meta::info double_reflection = ^^double;
+        constexpr std::meta::info string_reflection = ^^std::string;
+
+        typename [: int_reflection :] number = 100;
+        typename [: double_reflection :] pi = 3.14;
+        typename [: string_reflection :] message = "Hello Reflection";
+
+        std::cout << "Number: " << number << " of type " << typeid(number).name() << std::endl;
+        std::cout << "Pi: " << pi  << " of type " << typeid(pi).name() << std::endl;
+        std::cout << "Message: " << message  << " of type " << typeid(message).name() << std::endl;
+
+        // Number: 100
+        // Pi: 3.14
+        // Message: Hello Reflection
+    }
+
+
+    template <typename T>
+    void deduce_Type_of_Vector()
+    {
+        constexpr auto vec = ^^std::vector<T>;
+        typename [: vec :] x = { T(1), T(2), T(3)};
+        for (const auto& elem : x) {
+            std::cout << elem << " ";
+        }
+        std::cout << std::endl;
+
+        // Output: 1 2 3
+    }
+}
+
+
+namespace reflection::get_data_member_0
+{
+    void retrieve_Data_Members_Names()
+    {
+        // Reflect the type User
+        constexpr std::meta::info user_reflection = ^^User;
+
+        // Retrieve the name of the reflected type
+        std::cout << "Type: " << std::meta::identifier_of(user_reflection) << std::endl;
+
+        /*
+        // Reflect and list the non-static data members
+        [: expand(std::meta::nonstatic_data_members_of(user_reflection)) :] >>
+        [&]<std::meta::info member>() {
+            std::cout << " Member: " << std::meta::identifier_of(member) << std::endl;
+        };
+        */
+    }
+}
+
+namespace reflection::get_data_member_1
+{
+    consteval auto data_members_of(const std::meta::info& info)
+    {
+        constexpr auto ctx = std::meta::access_context::current();
+        return std::define_static_array(std::meta::nonstatic_data_members_of(info, ctx));
+    }
+
+    void demo()
+    {
+        template for( auto constexpr m: data_members_of(^^User) ) {   //***
+            println( "member named: {}", std::meta::display_string_of(m) );
+        }
+
+        /*
+        member named: reflection::User::name
+        member named: reflection::User::age
+        */
+    }
+}
+
+namespace reflection::enums
+{
+    void printValues_Simple()
+    {
+        template for(constexpr auto color : std::define_static_array(std::meta::enumerators_of(^^Color)))
+        {
+            constexpr std::string_view colorName = std::meta::identifier_of(color);
+            std::println("{}", colorName);
+        }
+
+        /*
+        Green
+        Blue
+        Yellow
+        Purple
+        */
+    }
+}
+
+namespace reflection::enums
 {
     template<typename E>
     requires std::is_enum_v<E>
@@ -387,7 +517,7 @@ namespace print_enum_values_2
     }
 
     template<typename E>
-    requires std::is_enum_v<E>
+        requires std::is_enum_v<E>
     const auto& to_string(E value)
     {
         static const auto toStringMap = [] static
@@ -410,10 +540,7 @@ namespace print_enum_values_2
         return s;
     }
 
-    enum class Color { Red, Green, Blue, Yellow, Purple };
-    enum class Animal { Cat, Dog, Horse, Rabbit, Snail };
-
-    int demo()
+    void printValues_Complex()
     {
         for(const auto color : enumerators_of<Color>())
             std::cout << color << std::endl;
@@ -422,27 +549,25 @@ namespace print_enum_values_2
 
         for(const auto animal : enumerators_of<Animal>())
             std::cout << animal << std::endl;
+
+        /*
+        Red
+        Green
+        Blue
+        Yellow
+        Purple
+
+        Cat
+        Dog
+        Horse
+        Rabbit
+        Snail
+        */
     }
-
-    /**
-    Red
-    Green
-    Blue
-    Yellow
-    Purple
-
-    Cat
-    Dog
-    Horse
-    Rabbit
-    Snail
-    **/
 }
-#endif
 
-namespace Reflection
+namespace reflection::enums
 {
-
     enum class Color
     {
         Transparent,
@@ -452,22 +577,20 @@ namespace Reflection
         Yellow
     };
 
-#if 0
-
-    .. https://compiler-explorer.com/z/W8b9xrx5j
+    // https://compiler-explorer.com/z/W8b9xrx5j
     // https://andreasfertig.com/blog/2025/08/cpp26-reflection-at-compile-time/?trk=feed_main-feed-card_feed-article-content
 
     template<typename E>
     requires std::is_enum_v<E>
-    constexpr inline auto num_enumerators_of{
-        std::meta::enumerators_of(^^E).size()};
+    constexpr inline auto num_enumerators_of {
+        std::meta::enumerators_of(^^E).size()
+    };
 
     template<typename E>
     requires std::is_enum_v<E>
-    consteval auto get_enum_values()
+    consteval auto get_enum_values() -> decltype(auto)
     {
         std::array<E, num_enumerators_of<E>> res;
-
         template for(size_t i{}; constexpr auto& e : std::define_static_array(std::meta::enumerators_of(^^E)))
         {
             res[i++] = [:e:];
@@ -475,54 +598,80 @@ namespace Reflection
         return res;
     }
 
-    void Test()
+    void enumerateValues()
     {
         for(const auto e : get_enum_values<Color>()) {
-            std::print("{} ", std::to_underlying(e));
+            std::println("{} ", std::to_underlying(e));
         }
-        std::println();
+
+        /**
+        0
+        2
+        3
+        8
+        9
+        **/
     }
-
-#endif
-
-};
-
-
-
-#if 0
-// https://godbolt.org/z/WeaKzWc56
-
-#include <iostream>
-#include <print>
-#include <experimental/meta>
-using namespace std;
-
-consteval auto data_members_of(meta::info info) {
-    constexpr auto ctx = meta::access_context::current();
-    return define_static_array(nonstatic_data_members_of(info, ctx));
 }
 
-struct MyOpts {
-    string fname;
-    int    count;   //***
-};
-
-auto main() -> int
+namespace reflection::checking_type
 {
-    template for( auto constexpr m: data_members_of(^^MyOpts) ) {   //***
-        println( "member named: {}", display_string_of(m) );
+    class A;
+
+    class B
+    {
+        int a;
+    };
+
+    void is_complete_type()
+    {
+        constexpr std::meta::info aInfo = ^^A;
+        constexpr std::meta::info bInfo = ^^B;
+
+        std::println("Is 'A' complete type: {}", std::meta::is_complete_type(aInfo));
+        std::println("Is 'B' complete type: {}", std::meta::is_complete_type(bInfo));
     }
 }
 
-Execution arguments...
-Program returned: 0
-Program stdout
-member named: fname
-member named: count
 
+namespace reflection::splicers
+{
+    void type_Aliases()
+    {
+        constexpr auto r = ^^int;
+        typename[:r:] x = 42;
 
+        static_assert(std::is_same_v<decltype(x), int>);
+    }
+}
 
-#endif
+namespace reflection::serialization
+{
+    struct Point2D
+    {
+        double x { 0.0 };
+        double y { 0.0 };
+    };
+
+    template <typename T>
+    void format(T const& t)
+    {
+        std::cout << identifier_of(^^T) << "{ ";
+        constexpr auto access_ctx = std::meta::access_context::unchecked();
+
+        template for (constexpr auto mem : std::define_static_array(nonstatic_data_members_of(^^T, access_ctx))) {
+            std::cout << std::format("{} = {}, ", identifier_of(mem), t.[:mem:]);
+        }
+
+        std::cout << "}\n";
+    }
+
+    void simpleType()
+    {
+        format(Point2D{.x = 3.0, .y = 4.0});
+    }
+}
+
 
 
 
@@ -531,7 +680,26 @@ int main([[maybe_unused]] int argc,
 {
     const std::vector<std::string_view> args(argv + 1, argv + argc);
 
-    // std::experimental::meta::info.
+    using namespace reflection;
+
+    types::basics();
+    types::reflectTypes_Simple();
+    types::useReflectedData();
+    types::deduce_Type_of_Vector<int>();
+    types::deduce_Type_of_Vector<double>();
+
+    // get_data_member_0::retrieve_Data_Members_Names();
+    // get_data_member_1::demo();
+
+    // enums::printValues_Simple();
+    // enums::printValues_Complex();
+    // enums::enumerateValues();
+
+    // checking_type::is_complete_type();
+
+    // splicers::type_Aliases();
+
+    // serialization::simpleType();
    
     return EXIT_SUCCESS;
 }
